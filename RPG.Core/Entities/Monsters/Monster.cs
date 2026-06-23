8f450c14684e7937a6d0255457dbbddc8cb1150b
Declan Noetzelman <declan@noetzelman.com>
2026-06-23 15:52:53 -0400

    feat(monster abstract class): create monster abstract class for enemy combatants

---
+++ RPG.Core/Entities/Monsters/Monster.cs
@@ -0,0 +1,25 @@
+namespace RPG.Core.Entities.Monsters;
+
+public abstract class Monster
+{
+    public int Id { get; protected set; }
+    public string Name { get; protected set; }
+    public int MaxHitpoints { get; protected set; }
+    public int CurrentHitpoints { get; protected set; }
+    public int ExperienceAwarded { get; protected set; }
+    public bool IsBoss { get; protected set; }
+
+    public Monster(string name)
+    {
+        Name = name;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        CurrentHitpoints -= damage;
+        if (CurrentHitpoints < 0)
+        {
+            CurrentHitpoints = 0;
+        }
+    }
+}

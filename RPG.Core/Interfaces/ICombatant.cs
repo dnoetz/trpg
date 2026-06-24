0840b01e0d4ada14ef073f06becd789fe770cf25
Declan Noetzelman <declan@noetzelman.com>
2026-06-23 21:27:19 -0400

    feat(interface): add icombatant interface to provide greater combat function

---
+++ RPG.Core/Interfaces/ICombatant.cs
@@ -0,0 +1,13 @@
+namespace RPG.Core.Interfaces;
+
+public interface ICombatant
+{
+    string Name { get; }
+    int CurrentHitpoints { get; }
+    int MaxHitpoints { get; }
+    bool IsDead { get; }
+
+    void TakeDamage(int damage);
+    int DealDamage();
+    void CombatDeath();
+}
